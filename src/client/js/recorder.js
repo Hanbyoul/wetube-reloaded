e72073d1fca5b341893131d704a8ec5685bb4ac7
Hanbyoul <dytjqdla120@naver.com>
2023-02-28 18:37:03 +0900

    ffmpeg

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,5 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
@@ -5,29 +7,50 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const handleDownload = async () => {
+  const ffmpeg = createFFmpeg({ log: true });
+  await ffmpeg.load();
+
+  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+
+  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+
+  await ffmpeg.run(
+    "-i",
+    "recording.webm",
+    "-ss", //추출을 시작할 위치
+    "00:00:01",
+    "-frames:v", //추출할 비디오 프레임 수
+    "1",
+    "thumbnail.jpg"
+  );
+
+  const mp4File = ffmpeg.FS("readFile", "output.mp4");
+  const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
+
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+  const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
+
+  const mp4Url = URL.createObjectURL(mp4Blob);
+  const thumbUrl = URL.createObjectURL(thumbBlob);
+
   const a = document.createElement("a");
-  //a태그 생성
-
-  a.href = videoFile;
-  //a태그 소스에 videoFile 넣기
-
-  a.download = "MyRecording.webm";
-  // a태그를 download 속성으로 설정
-  // 저장할때 자동으로 파일이름으로 설정됨.
-  // 파일의 확장자도(비디오포맷) 설정 가능
-
+  a.href = mp4Url;
+  a.download = "MyRecording.mp4";
   document.body.appendChild(a);
-  // html-body에 해당 a태그를 자식으로 넣는다
-
   a.click();
-  //a태그가 클릭된다 ( 다운로드가 자동으로 된다)
+
+  const thumbA = document.createElement("a");
+  thumbA.href = thumbUrl;
+  thumbA.download = "My Thumbnail.jpg";
+  document.body.appendChild(thumbA);
+  thumbA.click();
 };
 
 const handleStop = () => {
-  startBtn.innerText = "Download Recording"; // 하나의 버튼으로
-  startBtn.removeEventListener("click", handleStop); // 이벤트를 삭제
-  startBtn.addEventListener("click", handleDownload); // 이벤트를 생성
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
   recorder.stop();
 };
 
